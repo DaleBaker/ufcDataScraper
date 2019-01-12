d0e1b60913d571050e948cb655d471eb7e2e62c4
Dale <mightymuddy7@gmail.com>
2019-01-12 15:50:17 +1300

    tidied method, added gitignore

--- example.js
+++ example.js
@@ -10,24 +10,10 @@ let fightResults = {};
 
 let fightDetailsPromise = wikipedia_scraping.getFightDetails().then(function(value) {
 
-  let a = wikipedia_scraping.visitFightPages(0, value).then(function(value) {
-    console.log("\n\nhere we are");
-    console.log(value);
-    return value;
+    console.log("Links to UFC events obtained.");
+
+    let a = wikipedia_scraping.visitFightPages(0, value).then(function(value) {
+
+        return value;
   });
 });
-/*
-async function getExample() {
-    var resultA = await wikipedia_scraping.getFightDetails();
-
-    console.log(resultA);
-    console.log('here1');
-
-
-    var resultB = await wikipedia_scraping.visitFightPages(0, resultA);
-    console.log(resultB);
-    console.log('here2');
-
-    return resultA;// something using both resultA and resultB
-}
-getExample();*/
